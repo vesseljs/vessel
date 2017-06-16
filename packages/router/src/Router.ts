1ac0c0fe997765354599e8e1f8f0d6cc2f5ecf28
Pedro J. Martínez <pedrojmrt@gmail.com>
2017-06-16 19:12:20 +0200

    Router: render route

--- packages/router/src/Router.ts
+++ packages/router/src/Router.ts
@@ -1,3 +1,23 @@
+import { Vessel } from "@vessel/core";
+
 export class Router {
+
+    public renderRoute(routeName, ...args) {
+        let controller,
+            route,
+            cachedRoutes,
+            container = Vessel.$container,
+            metadataManager = container.get('@metadata_manager');
+
+        cachedRoutes = metadataManager.getRawData('cached_routes');
+        route = cachedRoutes.get(routeName);
+        controller = container.loadModule(route.context.constructor);
+
+        if (!route) {
+            throw new TypeError("Cannot render non-existent route: '"+routeName+"'.");
+        }
+
+        return route.bound.call(controller, ...args);
+    }
 
 }
